0e6290a93f01e36044c8597e8c90c6c7e19a2722
prograsshopper <tjalwled12@gmail.com>
2021-04-01 15:04:33 +0900

    VanillaJS: lecture 3.3

--- vanillaJS-in-2-weeks/greeting.js
+++ vanillaJS-in-2-weeks/greeting.js
@@ -1,26 +1,25 @@
 const form = document.querySelector(".js-form"),
     input = form.querySelector("input"),
-    greeting = document.querySelector(".js-greeting");
+    greeting = document.querySelector(".js-greetings");
 
 const USER_LS = "currentUser",
     SHOWING_CN = "showing";
 
 function saveName(text) {
     localStorage.setItem(USER_LS, text);
-    }
-    
-    function handleSubmit(event) {
+}
+
+function handleSubmit(event) {
     event.preventDefault();
     const currentValue = input.value;
     paintGreeting(currentValue);
     saveName(currentValue);
-    }
+}
     
-    function askForName() {
+function askForName() {
     form.classList.add(SHOWING_CN);
     form.addEventListener("submit", handleSubmit);
-    }
-      
+}
 
 function paintGreeting(text){
     form.classList.remove(SHOWING_CN);
@@ -38,6 +37,6 @@ function loadName(){
 }
 
 function init(){
-
+    loadName();
 }
 init();
